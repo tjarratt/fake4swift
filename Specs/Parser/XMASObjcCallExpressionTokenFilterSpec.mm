8a282f21d9eba84dd2b81256a0b795ec5924aa3c
Tim Jarratt <tjarratt@gmail.com>
2015-07-16 20:36:37 -0700

    Add a spec for call expressions that are arguments to another call expression

--- Specs/Parser/XMASObjcCallExpressionTokenFilterSpec.mm
+++ Specs/Parser/XMASObjcCallExpressionTokenFilterSpec.mm
@@ -11,6 +11,9 @@ describe(@"XMASObjcCallExpressionTokenFilter", ^{
     NSString *methodDeclarationFixture = [[NSBundle mainBundle] pathForResource:@"methodDeclaration" ofType:@"m"];
     NSArray *methodDeclarationTokens = [[CKTranslationUnit translationUnitWithPath:methodDeclarationFixture] tokens];
 
+    NSString *nestedCallExpressionsFixture = [[NSBundle mainBundle] pathForResource:@"NestedCallExpressions" ofType:@"m"];
+    NSArray *nestedCallExpressionTokens = [[CKTranslationUnit translationUnitWithPath:nestedCallExpressionsFixture] tokens];
+
     describe(@"-parseAllCallExpressionsFromTokens:", ^{
         XMASObjcCallExpressionTokenFilter *subject = [[XMASObjcCallExpressionTokenFilter alloc] init];
         it(@"should parse the range of each call expression in the array of tokens", ^{
@@ -21,6 +24,14 @@ describe(@"XMASObjcCallExpressionTokenFilter", ^{
                                          [NSValue valueWithRange:NSMakeRange(76, 6)],
                                          [NSValue valueWithRange:NSMakeRange(83, 4)],
                                          nil]);
+        });
+
+        it(@"should be able to parse nested call expressions", ^{
+            NSSet *callExprRanges = [subject parseCallExpressionRangesFromTokens:nestedCallExpressionTokens];
+            callExprRanges should contain([NSValue valueWithRange:NSMakeRange(129, 6)]);
+            callExprRanges should contain([NSValue valueWithRange:NSMakeRange(137, 6)]);
+            callExprRanges should contain([NSValue valueWithRange:NSMakeRange(145, 6)]);
+            callExprRanges should contain([NSValue valueWithRange:NSMakeRange(153, 6)]);
         });
     });
 });
