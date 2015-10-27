ccf63b66ff35bb397c077b0f92b7fe490548a7c2
Tim Jarratt <tjarratt@gmail.com>
2015-10-27 01:15:13 -0700

    SwiftProtocolProxy returns a protocol decl
    This should be useful when we get around to writing the templates
    for the fakes that implement a given protocol.
    
    Needs some unit tests around the parsing of the declaration, for sure.

---
+++ Specs/Fixtures/ProtocolEdgeCases.swift
@@ -0,0 +1,46 @@
+protocol MySpecialProtocol {
+    func voidMethod()
+    func randomDouble() -> Double
+    func randomDoubleWithSeed(seed: Int) -> Double
+    func multipleReturns() -> (Double, Double)
+
+    mutating func mutates()
+    static func isStatic()
+
+    // required initializer declaration
+    init(this: Int, orThat: Int)
+
+    subscript(subscriptAssignable : Int, areYouKidding : Float) -> Bool { get }
+    subscript(noWay : Bool) -> Bool { get set }
+
+    var numberOfWheels: Int { get }             //readonly
+    var numberOfSomething: Int { get set }      //read-write
+
+    // TODO : think about optionals
+    // TODO : think about non-nils
+    // e.g.: (does that affect params or return values?)
+
+    // TODO : think about "out" params (e.g.: NSError **)
+}
+
+@objc protocol MyOptionalProtocol {
+    optional func youMayOrMayNotImplementThis()
+}
+
+protocol IncludesOtherProtocol : MyOptionalProtocol, NSObjectProtocol { // this is right annoying
+
+}
+
+protocol ImplementableByClassesOnly : class {
+
+}
+
+// basically you can declare what type of ItemType you want at compile type
+// this is, in practice less powerful than C++ templates, but still pretty cool
+protocol GenericProtocolWithTypeAlias {
+    typealias ItemType
+    mutating func append(item: ItemType)
+    var count: Int { get }
+    subscript(i: Int) -> ItemType { get }
+}
+
